57d83c6233030c697ffa9c2a616d686c7660f002
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2021-02-23 12:05:32 +0545

    initialcommit

---
+++ venv/Lab1/labtwo.py
@@ -0,0 +1,8 @@
+# Write a program that reads the length of the base and the height of a right-angled triangle and print the area. Every number is given on a seperate line.
+
+length = int(Input("Enter the value of the length"))
+height = int(Intput("Enter the height"))
+
+area = (length * breadth)//2
+
+print(f"the area of triangle is {area}")
