832b715f06eccf92ffab2ff4eb1735864bcfd4a6
BibekMSDangol <78782357+BibekMSDangol@users.noreply.github.com>
2021-02-23 11:23:21 +0545

    Initial commit

---
+++ venv/Lab1/Lab.py
@@ -0,0 +1,6 @@
+#Write a program that takes three numbers and prints their sum. Every number is given on a seperate line.
+a = int (input("Enter the first number, "))
+b = int (input( "Enter the second number, "))
+c = int (input( "ENter the third number, "))
+sum_num = a+b+c
+print(f"The sum of given three numbers is : {sum_num}")
